e64d270124b0bf300872dbddeb0d27f232e5d9a5
mhmdsmdi <mhmd.samadi76@gmail.com>
2019-06-09 22:16:28 +0430

    data_handler add

---
+++ data_handler.py
@@ -0,0 +1,10 @@
+def read_csv(file_name):
+    return
+
+
+def generate_multi_class_data(num_class, num_feature, num_data, test_size):
+    return
+
+
+def generate_regression_date(num_feature, num_data, test_size):
+    return
